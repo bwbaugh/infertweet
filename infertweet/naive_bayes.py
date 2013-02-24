4d00c024b6bac7c7a8143452ec1e25c3eb83e83e
Wesley Baugh <wesley@bwbaugh.com>
2013-02-24 05:35:15 -0600

    Add Multinomial Naive Bayes classifier
    Naive Bayes is a simple classifier that is able to be updated in a
    streaming fashion (i.e. it can be updated with each new instance rather
    than having to iterate over the entire training set each time).
    Multinomial Naive Bayes uses word counts rather than a binary present or
    not-present value. This closes gh-3.
    
    Note that currently use the `Fraction` class to compute the *exact*
    probability. It might be more space and time efficient to use [log
    probability](https://en.wikipedia.org/wiki/Log_probability) instead.
    However, for small document lengths, such as we have with tweets, the
    slower time computing the exact may not matter.

---
+++ infertweet/naive_bayes.py
@@ -0,0 +1,101 @@
+# Copyright (C) 2013 Wesley Baugh
+"""Naive Bayes for text classification."""
+from collections import defaultdict
+from fractions import Fraction
+
+
+class MultinomialNB(object):
+    """Multinomial Naive Bayes for text classification.
+
+    Attributes:
+        label_vocab: Dictionary of sets of vocabulary by label.
+        label_count: Dictionary of times a label has been seen.
+        label_length: Dictionary of number of tokens seen in all
+            documents by label.
+        label_token_count: Dictionary of times a token has been seen by
+            label.
+    """
+    def __init__(self, *documents):
+        """Create a new Multinomial Naive Bayes classifier.
+        Args:
+            documents: Optional list of document-label pairs for training.
+        """
+        self.label_vocab = defaultdict(set)
+        self.label_count = defaultdict(int)
+        self.label_length = defaultdict(int)
+        self.label_token_count = defaultdict(lambda: defaultdict(int))
+        if documents:
+            self.train(*documents)
+
+    @property
+    def labels(self):
+        """Set of all class labels."""
+        return set(label for label in self.label_count)
+
+    @property
+    def vocabulary(self):
+        """Vocabulary across all class labels."""
+        label_vocab = [self.label_vocab[x] for x in self.label_vocab]
+        return set().union(*label_vocab)
+
+    def train(self, *documents):
+        """Train the classifier on a document-label pair(s).
+
+        Args:
+            documents: Tuple of (document, label) pair(s). Documents
+                must be a list of tokens, The label is a string for the
+                class label.
+        """
+        for document, label in documents:
+            self.label_count[label] += 1
+            # Python 3: isinstance(document, str)
+            if isinstance(document, basestring):
+                raise TypeError('Documents must be a list of tokens')
+            for token in document:
+                self.label_vocab[label].add(token)
+                self.label_token_count[label][token] += 1
+                self.label_length[label] += 1
+
+    def prior(self, label):
+        """Prior probability of a label."""
+        total = sum(self.label_count[x] for x in self.label_count)
+        return Fraction(self.label_count[label], total)
+
+    def conditional(self, token, label):
+        """Conditional probability for a token given a label."""
+        # Note we use [Laplace smoothing][laplace].
+        # [laplace]: https://en.wikipedia.org/wiki/Additive_smoothing
+
+        # Times token seen across all documents in a label.
+        numer = self.label_token_count[label][token] + 1
+        denom = self.label_length[label] + len(self.vocabulary)
+        return Fraction(numer, denom)
+
+    def score(self, document, label):
+        """Multinomial score of a document given a label."""
+        if isinstance(document, basestring):
+            raise TypeError('Documents must be a list of tokens')
+        score = self.prior(label)
+        for token in document:
+            score *= self.conditional(token, label)
+        return score
+
+    def _compute_scores(self, document):
+        """Compute the multinomial score of a document for all labels."""
+        return {x: self.score(document, x) for x in self.labels}
+
+    def prob_all(self, document):
+        """Probability of a document for all labels."""
+        score = self._compute_scores(document)
+        total = sum(score[x] for x in score)
+        return {label: Fraction(score[label], total) for label in self.labels}
+
+    def prob(self, document, label):
+        """Probability of a document given a label."""
+        prob = self.prob_all(document)[label]
+        return prob
+
+    def classify(self, document):
+        """Class label with maximum probability for a document."""
+        prob = self.prob_all(document)
+        return max(prob, key=prob.get)
---
+++ infertweet/tests/naive_bayes_test.py
@@ -0,0 +1,136 @@
+# Copyright (C) 2013 Wesley Baugh
+from fractions import Fraction
+from nose.tools import assert_raises
+
+from infertweet.naive_bayes import MultinomialNB
+
+
+class TestMultinomialNB(object):
+    # This test uses the examples provided by:
+    # http://nlp.stanford.edu/IR-book/pdf/13bayes.pdf
+    def setup(self):
+        self.training_docs = [
+                              ('Chinese Bejing Chinese', 'yes'),
+                              ('Chinese Chinese Shanghai', 'yes'),
+                              ('Chinese Macao', 'yes'),
+                              ('Tokyo Japan Chinese', 'no'),
+                             ]
+        self.training_docs = [(x.split(), y) for x, y in self.training_docs]
+        self.classifier = MultinomialNB(*self.training_docs)
+
+    def test_init_no_training(self):
+        classifier = MultinomialNB()
+        assert classifier.vocabulary == set()
+        assert classifier.labels == set()
+        classifier.train(*self.training_docs)
+        self.test_labels()
+        self.test_vocabulary()
+
+    def test_train_one_document(self):
+        documents = (['one', 'document', 'already', 'tokenized'], 'label')
+        classifier = MultinomialNB(documents)
+        expected = set(['one', 'document', 'already', 'tokenized'])
+        assert classifier.vocabulary == expected
+
+    def test_train_many_document(self):
+        documents = [(['one', 'document', 'already', 'tokenized'], 'label')] * 5
+        classifier = MultinomialNB(*documents)
+        expected = set(['one', 'document', 'already', 'tokenized'])
+        assert classifier.vocabulary == expected
+
+    def test_train_not_tokenized(self):
+        document = ('one document not tokenized', 'label')
+        assert_raises(TypeError, self.classifier.train, document)
+
+    def test_labels(self):
+        expected = set(['yes', 'no'])
+        assert self.classifier.labels == expected
+
+    def test_vocabulary(self):
+        expected = set(['Chinese', 'Bejing', 'Shanghai', 'Macao', 'Tokyo',
+                        'Japan'])
+        assert self.classifier.vocabulary == expected
+
+    def test_label_token_count(self):
+        tests = [
+                 ('yes', 'Chinese', 5),
+                 ('yes', 'Japan', 0),
+                 ('no', 'Chinese', 1),
+                 ('no', 'Japan', 1),
+                ]
+        for label, token, count in tests:
+            assert self.classifier.label_token_count[label][token] == count
+
+    def test_prior(self):
+        tests = [
+                 ('yes', Fraction(3, 4)),
+                 ('no', Fraction(1, 4)),
+                ]
+        for label, prob in tests:
+            assert self.classifier.prior(label) == prob
+
+    def test_conditional(self):
+        tests = [
+                  ('Chinese', 'yes', Fraction(6, 14)),
+                  ('Japan', 'yes', Fraction(1, 14)),
+                  ('Chinese', 'no', Fraction(2, 9)),
+                  ('Tokyo', 'no', Fraction(2, 9)),
+                  ('Japan', 'no', Fraction(2, 9)),
+                ]
+        for token, label, prob in tests:
+            assert self.classifier.conditional(token, label) == prob
+
+    def test_score(self):
+        tests = [
+                 ('Chinese Chinese Chinese Tokyo Japan', 'yes',
+                  Fraction(3, 4) * Fraction(3, 7) * Fraction(3, 7) *
+                  Fraction(3, 7) * Fraction(1, 14) * Fraction(1, 14)),
+                 ('Chinese Chinese Chinese Tokyo Japan', 'no',
+                  Fraction(1, 4) * Fraction(2, 9) * Fraction(2, 9) *
+                  Fraction(2, 9) * Fraction(2, 9) * Fraction(2, 9)),
+                ]
+        for document, label, score in tests:
+            result = self.classifier.score(document.split(), label)
+            assert result == score
+
+    def test_score_not_tokenized(self):
+        document, label = 'Chinese Chinese Chinese Tokyo Japan', 'yes'
+        assert_raises(TypeError, self.classifier.score, document, label)
+
+    def test_prob(self):
+        tests = [
+                 ('Chinese Chinese Chinese Tokyo Japan', 'yes',
+                  Fraction(4782969, 6934265)),
+                 ('Chinese Chinese Chinese Tokyo Japan', 'no',
+                  Fraction(2151296, 6934265)),
+                ]
+        for document, label, prob in tests:
+            result = self.classifier.prob(document.split(), label)
+            assert result == prob
+
+    def test_prob_not_tokenized(self):
+        document, label = 'Chinese Chinese Chinese Tokyo Japan', 'yes'
+        assert_raises(TypeError, self.classifier.prob, document, label)
+
+    def test_prob_all(self):
+        document = 'Chinese Chinese Chinese Tokyo Japan'
+        prob_all = self.classifier.prob_all(document.split())
+        tests = [
+                 ('yes', Fraction(4782969, 6934265)),
+                 ('no', Fraction(2151296, 6934265)),
+                ]
+        for label, prob in tests:
+            assert prob_all[label] == prob
+
+    def test_prob_all_not_tokenized(self):
+        document = 'Chinese Chinese Chinese Tokyo Japan'
+        assert_raises(TypeError, self.classifier.prob_all, document)
+
+    def test_classify(self):
+        document = 'Chinese Chinese Chinese Tokyo Japan'.split()
+        label = self.classifier.classify(document)
+        assert label == 'yes'
+
+    def test_classify_not_tokenized(self):
+        document = 'Chinese Chinese Chinese Tokyo Japan'
+        assert_raises(TypeError, self.classifier.classify, document)
